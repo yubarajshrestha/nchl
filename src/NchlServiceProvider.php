aaf5f810b8b1abe6add12c970dd3428421fcb966
Yubaraj Shreastha <companion.krish@outlook.com>
2019-12-29 21:51:38 +0545

    ServiceProvider and Facade Added

---
+++ src/NchlServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace YubarajShrestha\NCHL;
+
+use Illuminate\Support\ServiceProvider;
+use YubarajShrestha\NCHL\Services\NchlService;
+
+class NchlServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('nchl', function () {
+            return new NchlService();
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ src/Services/NchlService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace YubarajShrestha\NCHL\Services;
+
+class NchlService {
+    public function dump() {
+        dd('dumping something...');
+    }
+}
