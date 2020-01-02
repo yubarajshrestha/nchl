9bc7bb5d259408c3109bfff12be4001745bad273
Yubaraj Shreastha <companion.krish@outlook.com>
2020-01-03 00:12:09 +0545

    Form Added

--- src/Exceptions/NchlException.php
+++ src/Exceptions/NchlException.php
@@ -7,16 +7,41 @@ use YubarajShrestha\NCHL\Nchl;
 
 class NchlException extends Exception
 {
+    /**
+     * @var Nchl $subject
+     */
     public $subject;
 
     /**
-      * Report missing field.
-      */
+     * @param Nchl $subject
+     * @param $field
+     * @return NchlException
+     */
     public static function missingField(Nchl $subject, $field) {
         return (new static("Field `{$field}` is required"))->withSubject($subject);
     }
 
-    /** @return YubarajShrestha\NCHL\Exceptions\NchlException */
+    /**
+     * @param Nchl $subject
+     * @param $field
+     * @return NchlException
+     */
+    public static function certificateError(Nchl $subject, $field) {
+        return (new static($field))->withSubject($subject);
+    }
+
+    /**
+     * @param Nchl $subject
+     * @param $field
+     * @return NchlException
+     */
+    public static function propertyMissing(Nchl $subject, $field) {
+        return (new static($field))->withSubject($subject);
+    }
+
+    /**
+     * @return NchlException
+     */
     protected function withSubject() : NchlException {
         $this->subject;
         return $this;
--- src/Nchl.php
+++ src/Nchl.php
@@ -1,14 +1,14 @@
 <?php
+
 namespace YubarajShrestha\NCHL;
-use Exception;
-use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
 use YubarajShrestha\NCHL\Exceptions\NchlException;
 
 class Nchl {
 
     /** @var string */
     protected $merchant_id;
-    
+
     /** @var string */
     protected $app_id;
 
@@ -36,81 +36,232 @@ class Nchl {
     /** @var string */
     protected $particulars;
 
-    protected $config;
-    
+    protected $certificate;
+
     public function __construct(array $data = []) {
         $config = config('nchl');
         foreach($config as $key => $conf) {
             $this->$key = $conf;
         }
-        
         foreach ($data as $key => $value) {
             $this->$key = $value;
         }
+        $this->certificate = Storage::get('/public/certs/nchl.pfx');
+
+        $nchlData = [];
     }
 
-    public static function create(array $data = []) {
-        return new static($data);
+    /**
+     * Get NCHL merchant id
+     * @return string
+     */
+    public function getMerchantId(): string
+    {
+        return $this->merchant_id;
     }
 
-    public function merchant_id(string $merchant_id) {
+    /**
+     * @param string $merchant_id
+     */
+    public function setMerchantId(string $merchant_id): void
+    {
         $this->merchant_id = $merchant_id;
-        return $this;
     }
 
-    public function app_id(string $app_id) {
+    /**
+     * @return string
+     */
+    public function getAppId(): string
+    {
+        return $this->app_id;
+    }
+
+    /**
+     * @param string $app_id
+     */
+    public function setAppId(string $app_id): void
+    {
         $this->app_id = $app_id;
-        return $this;
     }
-    
-    public function app_name(string $app_name) {
+
+    /**
+     * @return string
+     */
+    public function getAppName(): string
+    {
+        return $this->app_name;
+    }
+
+    /**
+     * @param string $app_name
+     */
+    public function setAppName(string $app_name): void
+    {
         $this->app_name = $app_name;
-        return $this;
     }
 
-    public function txn_id(string $txn_id) {
+    /**
+     * @return string
+     */
+    public function getTxnId(): string
+    {
+        return $this->txn_id;
+    }
+
+    /**
+     * @param string $txn_id
+     */
+    public function setTxnId(string $txn_id): void
+    {
         $this->txn_id = $txn_id;
-        return $this;
     }
-    
-    public function txn_amount(string $txn_amount) {
-        $this->txn_amount = $txn_amount;
-        return $this;
+
+    /**
+     * @return string
+     */
+    public function getTxnDate(): string
+    {
+        return $this->txn_date;
     }
-    
-    public function txn_date(Carbon $txn_date) {
+
+    /**
+     * @param string $txn_date
+     */
+    public function setTxnDate(string $txn_date): void
+    {
         $this->txn_date = $txn_date;
-        return $this;
     }
-    
-    public function txn_currency(Carbon $txn_currency) {
+
+    /**
+     * @return string
+     */
+    public function getTxnCurrency(): string
+    {
+        return $this->txn_currency;
+    }
+
+    /**
+     * @param string $txn_currency
+     */
+    public function setTxnCurrency(string $txn_currency): void
+    {
         $this->txn_currency = $txn_currency;
-        return $this;
     }
 
-    public function reference_id(string $reference_id) {
+    /**
+     * @return string
+     */
+    public function getTxnAmount(): string
+    {
+        return $this->txn_amount;
+    }
+
+    /**
+     * @param string $txn_amount
+     */
+    public function setTxnAmount(string $txn_amount): void
+    {
+        $this->txn_amount = $txn_amount;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReferenceId(): string
+    {
+        return $this->reference_id;
+    }
+
+    /**
+     * @param string $reference_id
+     */
+    public function setReferenceId(string $reference_id): void
+    {
         $this->reference_id = $reference_id;
-        return $this;
     }
-    
-    public function remarks(string $remarks) {
+
+    /**
+     * @return string
+     */
+    public function getRemarks(): string
+    {
+        return $this->remarks;
+    }
+
+    /**
+     * @param string $remarks
+     */
+    public function setRemarks(string $remarks): void
+    {
         $this->remarks = $remarks;
-        return $this;
     }
-    
-    public function particulars(string $particulars) {
+
+    /**
+     * @return string
+     */
+    public function getParticulars(): string
+    {
+        return $this->particulars;
+    }
+
+    /**
+     * @param string $particulars
+     */
+    public function setParticulars(string $particulars): void
+    {
         $this->particulars = $particulars;
-        return $this;
     }
 
-    public function token() {
-        $message = "MERCHANTID={$this->merchant_id},APPID={$this->app_id},APPNAME={$this->app_name},TXNID=8024,TXNDATE=08-10-
-        2017,TXNCRNCY={$this->txn_currency},TXNAMT=1000,REFERENCEID=1.2.4,REMARKS=123455,PARTICULARS=12
-        345,TOKEN=TOKEN";
-
-        return $message;
+    /**
+     * @return string
+     */
+    public function getCertificate(): string
+    {
+        return $this->certificate;
     }
-    
+
+    /**
+     * @param string $certificate
+     */
+    public function setCertificate(string $certificate): void
+    {
+        $this->certificate = $certificate;
+    }
+
+    /**
+     * @return string
+     * @throws NchlException
+     */
+    public function token(): string {
+        $this->validate();
+        $string = "MERCHANTID={$this->merchant_id},APPID={$this->app_id},APPNAME={$this->app_name},TXNID={$this->txn_id},TXNAMT={$this->txn_amount}";
+//        $string = "MERCHANTID={$this->merchant_id},APPID={$this->app_id},APPNAME={$this->app_name},TXNID=8024,TXNDATE=08-10-
+//        2017,TXNCRNCY={$this->txn_currency},TXNAMT=1000,REFERENCEID=1.2.4,REMARKS=123455,PARTICULARS=12
+//        345,TOKEN=TOKEN";
+
+        $private_key = null;
+
+        if(openssl_pkcs12_read($this->certificate, $cert_info, "123")) {
+            $private_key = openssl_pkey_get_private($cert_info['pkey']);
+            //$array = openssl_pkey_get_details($private_key);
+            // print_r($array);
+        } else {
+            throw NchlException::certificateError($this,"Unable to read certificate.");
+        }
+
+        if(openssl_sign($string, $signature , $private_key, "sha256WithRSAEncryption")){
+            $hash = base64_encode($signature);
+            openssl_free_key($private_key);
+        } else {
+            throw NchlException::certificateError($this,"Unable to sign certificate.");
+        }
+
+        return $hash;
+    }
+
+    /**
+     * @throws NchlException
+     */
     public function validate() {
         $requiredFields = ['merchant_id', 'app_id', 'app_name', 'txn_currency', 'txn_id', 'txn_date', 'txn_amount', 'reference_id', 'remarks', 'particulars'];
         foreach ($requiredFields as $requiredField) {
@@ -120,10 +271,20 @@ class Nchl {
         }
     }
 
+    public function __serialize(): array
+    {
+        $array = get_object_vars($this);
+        $array['token'] = $this->token();
+        session(['nchl' => $array]);
+        unset($array['certificate']);
+        return $array;
+    }
+
+    /**
+     * @param $key
+     * @throws NchlException
+     */
     public function __get($key) {
-        if (! isset($this->$key)) {
-            throw new Exception("Property `{$key}` doesn't exist");
-        }
-        return $this->$key;
+        throw NchlException::propertyMissing($this, "Unable to access property: `{$key}`");
     }
 }
